543b8e1c8a3791a80a91217eb2c66ce4b501af5d
garethstorey20-lgtm <garethstorey20@gmail.com>
2026-06-08 19:08:21 +0800

    Update drive-config.js
    Added Google Drive API

--- web-pilot/drive-config.js
+++ web-pilot/drive-config.js
@@ -2,9 +2,6 @@
 // drive-config.js is optional if you pass ?dataId=... in the web address.
 
 window.DRIVE_PILOT_CONFIG = {
-  // Google Drive file ID for class-data.json (see README Step 4)
   fileId: "",
-
-  // Strongly recommended for phones/tablets/browsers (see README Step 5)
-  apiKey: "",
+  apiKey: "AIzaSyDVmKapHCxPx3PRrkFZz7wlUvJm1WfVpMA",
 };
